0b6a9e91024c66f2f027e9cf55ec74095b42b877
J0-J0 <726692245@qq.com>
2019-06-11 18:20:23 +0800

    bug修复

--- demoUtil/src/main/java/com/jojo/zzz/Work.java
+++ demoUtil/src/main/java/com/jojo/zzz/Work.java
@@ -1,10 +1,25 @@
-
 package com.jojo.zzz;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Work {
 
     public static void main(String[] args) {
-
+        int count = 10000000;
+        ConcurrentLinkedQueue<Integer> queue = new ConcurrentLinkedQueue<>();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> {
+            for (int i = 0; i < count; i++) {
+                queue.add(1);
+            }
+        });
+        //
+        for (int i = 0; i < count; i++) {
+            System.out.println("第" + i + "次" + queue.poll());
+        }
+        executorService.shutdown();
     }
 
 }
