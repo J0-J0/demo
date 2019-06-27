81bd37d2584db61584164b4265f2547b6a3b9bcd
J0-J0 <726692245@qq.com>
2019-06-27 17:27:53 +0800

    测试类备份； 动态日志等级；

---
+++ demoSSM/src/main/java/com/jojo/pojo/LoggerDTO.java
@@ -0,0 +1,22 @@
+package com.jojo.pojo;
+
+public class LoggerDTO {
+    private String name;
+    private String level;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+}
--- demoSSM/src/main/java/com/jojo/pojo/Response.java
+++ demoSSM/src/main/java/com/jojo/pojo/Response.java
@@ -2,6 +2,9 @@ package com.jojo.pojo;
 
 public class Response {
 
+    public static final int SUCCESS = 1;
+    public static final int FAIL = 0;
+
     private int code;
     private String message;
     private Object data;
@@ -36,5 +39,20 @@ public class Response {
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+    public void setFailMessage(String message) {
+        this.code = FAIL;
+        this.message = message;
+    }
+
+    public void setSuccessMessage(String message) {
+        this.code = SUCCESS;
+        this.message = message;
+    }
+
+    public Response appendMessage(String message) {
+        this.message += message;
+        return this;
     }
 }
